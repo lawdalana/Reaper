2d92b947a03c6896ffa210d2b777e7c727a8c31c
JN <wasawat@dek-d.com>
2017-06-04 01:32:12 +0700

    Add core file

---
+++ Core/config.php
@@ -0,0 +1,16 @@
+<?php
+
+/*Define Database */
+define('DB_Host','localhost');
+define('DB_User','root');
+define('DB_Pass','');
+define('DB_Name','test');
+define('DB_Info','profile');
+
+/*Show error to View*/
+// $show_error = true;
+
+/*Write error to this path*/
+// $error_file_path = "../logs/log.txt";
+
+?>
---
+++ Core/init.php
@@ -0,0 +1,10 @@
+<?php
+	require_once("../vendor/autoload.php");
+	require_once("config.php");
+	// $test = new Model\database();
+
+	// echo "{$DB_Host} Hello";
+	// $loader = require  '../vendor/autoload.php';
+	// var_dump($loader);
+
+?>
