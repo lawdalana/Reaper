8549238d6a96d686adf2fc55d53f999fb5d90059
JN <wasawat@dek-d.com>
2017-06-04 01:22:19 +0700

    Test and Try define, database

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,9 +6,33 @@ namespace Composer\Autoload;
 
 class ComposerStaticInitdb30fa10150ee68181f2908797def030
 {
+    public static $prefixLengthsPsr4 = array (
+        'M' => 
+        array (
+            'Model\\' => 6,
+        ),
+        'C' => 
+        array (
+            'Core\\' => 5,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'Model\\' => 
+        array (
+            0 => __DIR__ . '/../..' . '/Model',
+        ),
+        'Core\\' => 
+        array (
+            0 => __DIR__ . '/../..' . '/Core',
+        ),
+    );
+
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
+            $loader->prefixLengthsPsr4 = ComposerStaticInitdb30fa10150ee68181f2908797def030::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInitdb30fa10150ee68181f2908797def030::$prefixDirsPsr4;
 
         }, null, ClassLoader::class);
     }
