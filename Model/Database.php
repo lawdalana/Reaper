aa2047d0b9b6db48b1e901371038945ffac8094b
JN <wasawat@dek-d.com>
2017-06-04 01:20:43 +0700

    Add method search by id and query usd PDO

--- Model/Database.php
+++ Model/Database.php
@@ -1,15 +1,39 @@
 <?php
-namespace Model
+namespace Model;
+use PDO;
+require_once("../Core/config.php");
+
 class database{
-	public $connection;
+	private $connection;
 
 	function __construct(){
 		$this->connect_db();
 	}
 
 	function connect_db(){
-		$connection = PDO();
+		try{
+			$connection = new PDO("mysql:host=". DB_Host .";dbname=".DB_Name , DB_User, DB_Pass);
+		}catch (PDOException $e){
+			echo $e->getMessage();
+		}
+		
+		
 	}
+
+	public function query($sql, $data_array){
+		if(is_array($data_array)){
+			$statement = $connection->prepare(sql);
+			return $statement->exec($data_array);
+		}
+		
+	}
+
+	public function query_by_id($id){
+		$statement = $connection->prepare("SELECT * FROM ". DB_Info ."WHERE ID = :ID");
+		$data = array(':ID' => $id );
+		return $statement->exec($data_array);
+	}
+
 }
 
 ?>
