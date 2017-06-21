6ac6cf8aacab23af6359448f300b0ed3be0209c0
JN <wasawat@dek-d.com>
2017-06-22 00:36:35 +0700

    fix database and Add show-item

--- Q.php
+++ Q.php
@@ -1,11 +1,11 @@
 <?php
 $host = "localhost";
 $username = "root";
-$password = "1234";
+$password = "";
 $tablename = "test";
-$connection = mysqli_connect($servername, $username, $password, $tabfelename);
-// $sql = "select id,username,title,fav from result where title like '%".$_POST['search']."%'";
-$sql = "select id,username,title,fav from result where title like '%F%'";
+$connection = mysqli_connect( $host, $username, $password, $tablename);
+$sql = "select ID,FirstName,LastName,ItemID from profile where Firstname like '%".$_POST['search']."%'";
+// $sql = "select id,username,title,fav from result where title like '%F%'";
 // AND updated > date("2017-05-20")
 // AND published="N"
 // AND chapter>=28
@@ -16,18 +16,18 @@ if(mysqli_num_rows($result)>0){
   $output .= "<table class='table table-bordered'
         <tr>
           <th>ID</th>
-          <th>Username</th>
-          <th>Title</th>
-          <th>Fav</th>
+          <th>FirstName</th>
+          <th>LastName</th>
+          <th>ItemID</th>
         </tr>
   ";
   while($tmp = mysqli_fetch_assoc($result)) {
-    $output.="<tr><td>".$row["id"]."</td><td>".$row["username"]."</td><td>".$row["title"]."</td><td>".$row["fav"]."</td></tr>".
+    $output.="<tr><td>".$tmp["ID"]."</td><td>".$tmp["FirstName"]."</td><td>".$tmp["LastName"]."</td><td>".$tmp["ItemID"]."</td></tr>";
 }
     $output.="</table>";
     echo $output;
 }
 else{
-  echo "Data not found<br>"
+  echo "Data not found<br>";
 }
 ?>
